60112d53145584eefa1786d9f09433a40db1b6ce
NoahJGersh <me@noahger.sh>
2025-04-10 22:07:28 -0700

    feat(poke-utils): implement location areas helper

---
+++ internal/poke-utils/go.mod
@@ -0,0 +1,3 @@
+module poke-utils
+
+go 1.23.4
---
+++ internal/poke-utils/main.go
@@ -0,0 +1,50 @@
+package pokeutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type namedAPIResource struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
+type locationAreasBody struct {
+	Count    int                `json:"count"`
+	Next     string             `json:"next"`
+	Previous string             `json:"previous"`
+	Results  []namedAPIResource `json:"results"`
+}
+
+func GetLocationAreas(url string) (locationAreas []string, next, previous string, err error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return []string{}, "", "", fmt.Errorf("no response from API: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return []string{}, "", "", fmt.Errorf("could not read response body: %w", err)
+	}
+
+	if res.StatusCode > 299 {
+		return []string{}, "", "", fmt.Errorf("response failed with status code: %d and\nbody: %s", res.StatusCode, body)
+	}
+
+	areas := locationAreasBody{}
+	err = json.Unmarshal(body, &areas)
+	if err != nil {
+		return []string{}, "", "", fmt.Errorf("could not unmarshal body: %w", err)
+	}
+
+	var areaNames []string
+	for _, area := range areas.Results {
+		areaNames = append(areaNames, area.Name)
+	}
+
+	return areaNames, areas.Next, areas.Previous, nil
+}
